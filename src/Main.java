502a3049ebd64bb11504239de6043869db1802b8
Пользователь <777draft777@mail.ru>
2026-05-31 13:23:01 +0300

    Задание 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,32 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Задание 1");
+        int age = 15;
+        if (age >= 18) {
+            System.out.println("Если возраст человека равен " + age + " лет,то он совершеннолетний");
         }
+        if (age < 18) {
+            System.out.println("Если возраст человека равен " + age + " лет,то он не достиг совершеннолетия,нужно подождать");
+        }
+
+        System.out.println("Задание 2");
+        int airTemperature = 6;
+        if (airTemperature<=5){
+            System.out.println("На улице холодно,нужно надеть шапку");
+        }
+        if (airTemperature >5){
+            System.out.println("Сегодня тепло,можно идти без шапки");
+        }
+
+        System.out.println("Задание 3");
+        int speed = 75;
+        if(speed<=60){
+            System.out.println("Если скорость " + speed + " км/ч,то можно ездить спокойно");
+        } else {
+            System.out.println("Если скорость " + speed + " км/ч,то придется заплатить штраф");
+        }
+
     }
 }
